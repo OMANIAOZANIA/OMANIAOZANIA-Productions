79bbc6c8c9741d24cf2a774c2af4dd0c62c291f3
OMANIAOZANIA <116654808+OMANIAOZANIA@users.noreply.github.com>
2022-11-18 16:18:02 +0700

    Add files via upload

---
+++ GitHub_LOADER.lua
@@ -0,0 +1,42 @@
+local GITHUB_REPOSITORY = "https://github.com/OMANIAOZANIA/OMANIAOZANIA-Productions"
+local GITHUB = {}
+
+local getasset = getsynasset or getcustomasset
+local httpRequest = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request
+
+local FUNCTION_isFile = function(file)
+    local success, response = pcall(function() return readfile(file) end)
+    return success and response ~= nil
+end
+
+function GITHUB:GetCustomAsset(assetPath, cacheAsset)
+-- assetPath: github repo (https://raw.githubusercontent.com/OMANIAOZANIA/OMANIAOZANIA-Productions/main/...)
+-- assetPath: workspace ($ExploitFolder$/workspace/...)
+    spawn(function()
+        if (not cacheAsset) then return getasset(URLasset) end
+
+        if (not FUNCTION_isFile(assetPath)) then
+            spawn(function()
+				local loadText = Instance.new("TextLabel")
+				loadText.Size = UDim2.new(1, 0, 0, 36)
+				loadText.Text = "Downloading " .. assetPath
+				loadText.BackgroundTransparency = 1
+				loadText.TextStrokeTransparency = 0
+				loadText.TextSize = 30
+				loadText.Font = Enum.Font.SourceSans
+				loadText.TextColor3 = Color3.new(1, 1, 1)
+				loadText.Position = UDim2.new(0, 0, 0, -36)
+				loadText.Parent = game.CoreGui
+				repeat wait() until FUNCTION_isFile(assetPath)
+				loadText:Remove()
+			end)
+            local requestAsset = httpRequest({
+                Url = "https://raw.githubusercontent.com/OMANIAOZANIA/OMANIAOZANIA-Productions/main/" .. assetPath:gsub("OMANIA_Productions/", ""),
+                Method = "GET"
+            })
+            writefile(assetPath, requestAsset.Body)
+        end
+    end)
+end
+
+return GITHUB
