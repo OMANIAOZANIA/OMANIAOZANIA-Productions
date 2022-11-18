22020a6db890e2b3778d6689bf74cb25c89e962e
OMANIAOZANIA <116654808+OMANIAOZANIA@users.noreply.github.com>
2022-11-18 17:38:54 +0700

    Update GitHub_LOADER.lua

--- GitHub_LOADER.lua
+++ GitHub_LOADER.lua
@@ -4,9 +4,23 @@ local GITHUB = {}
 local getasset = getsynasset or getcustomasset
 local httpRequest = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request
 
-local FUNCTION_isFile = function(file)
-    local success, response = pcall(function() return readfile(file) end)
+local FUNCTION_isFile = function(filePath)
+    local success, response = pcall(function() return readfile(filePath) end)
     return success and response ~= nil
+end
+local FUNCTION_writeFile = function(filePath, fileContent)
+    local folders = filePath:split("/")
+    for index, folder in pairs(folders) do
+        if (index > 1) then
+            for previous = index - 1, 1, -1 do
+                folder = folders[previous] .. "/" .. folder
+            end
+        end
+        if (index >= #folders) then writefile(folder, fileContent) break end
+
+        if (not isfolder(folder)) then
+            makefolder(folder) end
+    end
 end
 
 function GITHUB:GetCustomAsset(assetPath, cacheAsset)
@@ -16,8 +30,8 @@ function GITHUB:GetCustomAsset(assetPath, cacheAsset)
         return getasset(GITHUB_REPOSITORY .. assetPath:gsub("OMANIA_Productions/", ""))
     end
 
-    local assetSavePath
-    if (not FUNCTION_isFile(assetPath)) then
+    local assetSavePath 
+    if (not FUNCTION_isFile(assetPath:gsub("%%20", " "))) then
         spawn(function()
             local loadText = Instance.new("TextLabel")
             loadText.Size = UDim2.new(1, 0, 0, 36)
@@ -37,10 +51,11 @@ function GITHUB:GetCustomAsset(assetPath, cacheAsset)
             Method = "GET"
         })
         assetSavePath = assetPath:gsub("%%20", " ")
-        writefile(assetSavePath, requestAsset.Body)
+        FUNCTION_writeFile(assetSavePath, requestAsset.Body)
     end
-    print(assetSavePath)
+    if (not assetSavePath) then
+        return getasset(assetPath:gsub("%%20", " ")) end
     return getasset(assetSavePath)
 end
-
+GITHUB:GetCustomAsset("OMANIA_Productions/BedWars%20WEED/assets/BedTP.png", true)
 return GITHUB
