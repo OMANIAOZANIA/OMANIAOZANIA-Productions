6c11fea53076d3fa6534bc6ab3a1473dbcd32dbc
OMANIAOZANIA <116654808+OMANIAOZANIA@users.noreply.github.com>
2022-11-18 17:07:24 +0700

    Update GitHub_LOADER.lua

--- GitHub_LOADER.lua
+++ GitHub_LOADER.lua
@@ -12,37 +12,35 @@ end
 function GITHUB:GetCustomAsset(assetPath, cacheAsset)
 -- assetPath: github repo (https://raw.githubusercontent.com/OMANIAOZANIA/OMANIAOZANIA-Productions/main/...)
 -- assetPath: workspace ($ExploitFolder$/workspace/...)
-    spawn(function()
-        if (not cacheAsset) then
-            return getasset(GITHUB_REPOSITORY .. assetPath:gsub("OMANIA_Productions/", ""))
-        end
+    if (not cacheAsset) then
+        return getasset(GITHUB_REPOSITORY .. assetPath:gsub("OMANIA_Productions/", ""))
+    end
 
-        local assetSavePath
-        if (not FUNCTION_isFile(assetPath)) then
-            spawn(function()
-				local loadText = Instance.new("TextLabel")
-				loadText.Size = UDim2.new(1, 0, 0, 36)
-				loadText.Text = "Downloading " .. assetPath
-				loadText.BackgroundTransparency = 1
-				loadText.TextStrokeTransparency = 0
-				loadText.TextSize = 30
-				loadText.Font = Enum.Font.SourceSans
-				loadText.TextColor3 = Color3.new(1, 1, 1)
-				loadText.Position = UDim2.new(0, 0, 0, -36)
-				loadText.Parent = game.CoreGui
-				repeat wait() until FUNCTION_isFile(assetPath)
-				loadText:Remove()
-			end)
-            local requestAsset = httpRequest({
-                Url = GITHUB_REPOSITORY  .. assetPath:gsub("OMANIA_Productions/", ""),
-                Method = "GET"
-            })
-            assetSavePath = assetPath:gsub("%%20", " ")
-            writefile(assetSavePath, requestAsset.Body)
-        end
-	    print(assetSavePath)
-        return getasset(assetSavePath)
-    end)
+    local assetSavePath
+    if (not FUNCTION_isFile(assetPath)) then
+        spawn(function()
+            local loadText = Instance.new("TextLabel")
+            loadText.Size = UDim2.new(1, 0, 0, 36)
+            loadText.Text = "Downloading " .. assetPath
+            loadText.BackgroundTransparency = 1
+            loadText.TextStrokeTransparency = 0
+            loadText.TextSize = 30
+            loadText.Font = Enum.Font.SourceSans
+            loadText.TextColor3 = Color3.new(1, 1, 1)
+            loadText.Position = UDim2.new(0, 0, 0, -36)
+            loadText.Parent = game.CoreGui
+            repeat wait() until FUNCTION_isFile(assetPath)
+            loadText:Remove()
+        end)
+        local requestAsset = httpRequest({
+            Url = GITHUB_REPOSITORY  .. assetPath:gsub("OMANIA_Productions/", ""),
+            Method = "GET"
+        })
+        assetSavePath = assetPath:gsub("%%20", " ")
+        writefile(assetSavePath, requestAsset.Body)
+    end
+    print(assetSavePath)
+    return getasset(assetSavePath)
 end
 
 return GITHUB
